dbc8f4f01c6ab6ecc6f3acd4876ce7f133931ec7
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-10 17:51:26 +0000

    t0

--- sort.h
+++ sort.h
@@ -15,5 +15,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
 void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
 #endif 
