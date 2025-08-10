a2799ca57a85786ff67a456b30125daae7769b83
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-10 18:17:32 +0000

    t1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,36 +7,36 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *tmp;
-
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    current = (*list)->next;
-
-    while (current)
-    {
-        tmp = current;
-        current = current->next;
-
-        while (tmp->prev && tmp->n < tmp->prev->n)
-        {
-
-            tmp->prev->next = tmp->next;
-            if (tmp->next)
-                tmp->next->prev = tmp->prev;
-
-            tmp->next = tmp->prev;
-            tmp->prev = tmp->prev->prev;
-
-            if (tmp->prev)
-                tmp->prev->next = tmp;
-            else
-                *list = tmp;
-
-            tmp->next->prev = tmp;
-
-            print_list(*list);
-        }
-    }
+	listint_t *current, *tmp;
+	
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	
+	current = (*list)->next;
+	
+	while (current)
+	{
+		tmp = current;
+		current = current->next;
+		
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			
+			tmp->prev->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+			
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			
+			tmp->next->prev = tmp;
+			
+			print_list(*list);
+		}
+	}
 }
