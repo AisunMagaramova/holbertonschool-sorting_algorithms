b444861df259059bd0879c9a3e396bd822376f98
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-10 18:01:20 +0000

    t0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,10 +11,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 	int swapped;
-	
+
 	if (array == NULL || size < 2)
 		return;
-	
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
@@ -25,12 +25,12 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				
+
 				print_array(array, size);
 				swapped = 1;
 			}
 		}
-		
+
 		if (!swapped)
 			break;
 	}
